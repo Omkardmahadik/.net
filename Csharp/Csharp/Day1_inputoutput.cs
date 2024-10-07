e20785b2fee69721a85c46c5a1a4e2d2af29998d
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-07 10:26:41 +0530

    commit C#

---
+++ Csharp/Csharp/Day1_inputoutput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Day1_inputoutput
+    {
+        static void Main(string[] args)
+        {
+            //output
+            Console.WriteLine("Hello csharp");
+
+            //how to take value from user i/o
+            int n1, n2, sum;
+            Console.WriteLine("enter 2no:");
+            n1=Convert.ToInt32(Console.ReadLine());
+            n2=Convert.ToInt32(Console.ReadLine());
+            sum=n1 + n2;
+            Console.WriteLine("sum{0}",sum);
+
+            //if we declare if in variable and how we use in if?
+            //@ allows the keywords to be used as a variable
+            string @if = @"hello omkar";
+            Console.WriteLine(@if);
+
+            //type casting
+            float rate = 10;
+            Console.WriteLine(rate);
+            int a = (int)10.7f;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Csharp/Csharp/Day2_ifelse.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Day2_ifelse
+    {
+        static void Main(string[] args)
+        {
+            //if
+            /* int no;
+              Console.WriteLine("enter no:");
+              no = Convert.ToInt32(Console.ReadLine());
+
+              if (no%2==0) {
+                  Console.WriteLine("even");
+              }
+              else
+              {
+                  Console.WriteLine("odd");
+              }
+            */
+            //if else
+            /*
+            int person;
+            Console.WriteLine("enter your age:");
+            person = Convert.ToInt32(Console.ReadLine());
+            if (person>=18)
+            {
+                Console.WriteLine("person is eligible");  
+            }
+            else
+            {
+                Console.WriteLine("person is not eligible");
+            }
+            */
+
+            //if-else chain
+            int student;
+
+            Console.WriteLine("enter your marks");
+            student = Convert.ToInt32(Console.ReadLine());
+
+            if (student>=90)
+            {
+                Console.WriteLine("grade:A");
+            }
+            else if(student>=80 && student <= 89)
+            {
+                Console.WriteLine("grade:B");
+            }
+            else if (student >= 70 && student <= 79)
+            {
+                Console.WriteLine("grade:C");
+            }
+            else if (student >= 60 && student <= 69)
+            {
+                Console.WriteLine("grade:D");
+            }
+            else
+            {
+                Console.WriteLine("you are fail");
+            }
+        }
+    }
+}
